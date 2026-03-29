04fd8bfbf78e07c5c16b480ae93fadc1d1d1d3d8
Mr-Saroj <sarojaojha09@gmail.com>
2026-03-29 12:44:18 +0530

    debug3

--- Backend-servicehub/Backend-servicehub/src/main/java/in/sp/main/config/CorsConfig.java
+++ Backend-servicehub/Backend-servicehub/src/main/java/in/sp/main/config/CorsConfig.java
@@ -1,9 +1,9 @@
 package in.sp.main.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.*;
 import org.springframework.web.filter.CorsFilter;
 
 import java.util.Arrays;
@@ -11,12 +11,20 @@ import java.util.Arrays;
 @Configuration
 public class CorsConfig {
 
+    @Value("${FRONTEND_URL}")
+    private String frontendUrl;
+
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+
+        config.setAllowedOrigins(Arrays.asList(
+                "http://localhost:5173",
+                frontendUrl   // ✅ dynamic
+        ));
+
         config.setAllowedHeaders(Arrays.asList("*"));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 
