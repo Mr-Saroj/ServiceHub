84938d7b9df06ff417c1252ea7880843af0f8f4c
Mr-Saroj <sarojaojha09@gmail.com>
2026-04-06 20:57:36 +0530

    REmove dot env code

--- Backend-servicehub/Backend-servicehub/src/main/java/in/sp/main/config/CloudinaryConfig.java
+++ Backend-servicehub/Backend-servicehub/src/main/java/in/sp/main/config/CloudinaryConfig.java
@@ -2,22 +2,29 @@ package in.sp.main.config;
 
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
-import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CloudinaryConfig {
 
+    @Value("${CLOUDINARY_CLOUD_NAME}")
+    private String cloudName;
+
+    @Value("${CLOUDINARY_API_KEY}")
+    private String apiKey;
+
+    @Value("${CLOUDINARY_API_SECRET}")
+    private String apiSecret;
+
     @Bean
     public Cloudinary cloudinary() {
 
-        Dotenv dotenv = Dotenv.load();
-
         return new Cloudinary(ObjectUtils.asMap(
-                "cloud_name", dotenv.get("CLOUDINARY_CLOUD_NAME"),
-                "api_key", dotenv.get("CLOUDINARY_API_KEY"),
-                "api_secret", dotenv.get("CLOUDINARY_API_SECRET")
+                "cloud_name", cloudName,
+                "api_key", apiKey,
+                "api_secret", apiSecret
         ));
     }
 }
